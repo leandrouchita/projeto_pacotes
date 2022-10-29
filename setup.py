0fb8e70e0d48ca0fd2055f36a5fdb454978269fe
leandrouchita <114877256+leandrouchita@users.noreply.github.com>
2022-10-29 20:32:10 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Image_processing_dio_teste",
+    version="0.0.3",
+    author="Leandro",    
+    description="Image Processing Package using Skimage", 
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
